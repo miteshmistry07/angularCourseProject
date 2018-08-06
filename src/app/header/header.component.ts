05c70f8fc19e4399e4fd3c94391a4cc179a004e3
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-08-06 14:06:02 +0100

    compontents added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+
+}
