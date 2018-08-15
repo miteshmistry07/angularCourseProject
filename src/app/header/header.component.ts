3b7c7ee2abaf96e4a1972c6e5df8b8489af2ce8b
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-08-15 09:16:25 +0100

    start of adding navigation and event binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "../../../node_modules/@angular/core";
+import { Component, EventEmitter, Output } from "../../../node_modules/@angular/core";
+
 
 @Component({
     selector: 'app-header',
@@ -6,5 +7,10 @@ import { Component } from "../../../node_modules/@angular/core";
 })
 export class HeaderComponent {
 
+    @Output() featureSelected = new EventEmitter<string>(); //listen from the parent component
+
+    onSelect(feature:string) {
+        this.featureSelected.emit(feature);
+    }
 
 }
