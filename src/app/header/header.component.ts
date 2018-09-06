bbd7657475fa76eedf62a96b3d7f4adb322c830c
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-09-06 14:31:29 +0100

    completed routing module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "../../../node_modules/@angular/core";
+import { Component } from "../../../node_modules/@angular/core";
 
 
 @Component({
@@ -6,11 +6,5 @@ import { Component, EventEmitter, Output } from "../../../node_modules/@angular/
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
-    @Output() featureSelected = new EventEmitter<string>(); //listen from the parent component
-
-    onSelect(feature:string) {
-        this.featureSelected.emit(feature);
-    }
 
 }
