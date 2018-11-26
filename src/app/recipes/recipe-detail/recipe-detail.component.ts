0337e88af91b5e6702c30b745a781e84eb28141d
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-11-26 16:22:08 +0000

    completed forms exercise

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -36,4 +36,9 @@ export class RecipeDetailComponent implements OnInit {
     this.router.navigate(['edit'], {relativeTo: this.route}); //2 ways of doing it this is the easier way
     // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route } );
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 export class ShoppingListService {
     
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEdited = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -12,6 +13,10 @@ export class ShoppingListService {
 
     getIngredients() {
         return this.ingredients.slice();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient) {
@@ -26,6 +31,16 @@ export class ShoppingListService {
         // ^^ would cause alot of events to be emitted and for larger apps not suitable
 
         this.ingredients.push(...ingredients); //turns array into a list of elements
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
